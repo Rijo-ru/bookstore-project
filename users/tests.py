de73d48010e6a8192db63990845dd014141e7176
Eugine <Rijo@mail.ru>
2020-12-06 20:52:45 +1000

    user app tests added

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+
+class CustomerUserTests(TestCase):
+
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='test_user',
+            email='test@mail.com',
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'test_user')
+        self.assertEqual(user.email, 'test@mail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='test_superuser',
+            email='test_superuser@mail.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'test_superuser')
+        self.assertEqual(admin_user.email, 'test_superuser@mail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
