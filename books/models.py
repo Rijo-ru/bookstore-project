3c8b5803ee419b9bd34fb1c5ad4f9d2b8e6ebc13
Eugine <Rijo@mail.ru>
2020-12-15 16:50:49 +1000

    Review app added. Not complited

--- books/models.py
+++ books/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.urls import reverse
 import uuid
+from django.contrib.auth import get_user_model
 
 
-# Create your models here.
 class Book(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
@@ -15,3 +15,13 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews',)
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return self.review
+
