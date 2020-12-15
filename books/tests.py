e7f1b98b1c38ce42f2127757ebf5e0e5590379f0
Eugine <Rijo@mail.ru>
2020-12-15 12:28:48 +1000

    tests added in books app

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Book
+
+
+class BookTest(TestCase):
+
+    def setUp(self):
+        self.book = Book.objects.create(title='Harry Potter', author='JK Rowling', price='25.00')
+
+    def test_book_listing(self):
+        self.assertEqual(f'{self.book.title}', 'Harry Potter')
+        self.assertEqual(f'{self.book.author}', 'JK Rowling')
+        self.assertEqual(f'{self.book.price}', '25.00')
